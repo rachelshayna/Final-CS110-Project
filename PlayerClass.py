319719ec36077f98680fe48bce4e7c2511559968
kylamel <kylameltzer@gmail.com>
2017-04-21 15:34:59 -0400

    Update PlayerClass.py

--- PlayerClass.py
+++ PlayerClass.py
@@ -1,13 +1,15 @@
-import time
-import random
 import pygame
-import threading
 
-class Player:
-    def __init__(self,xcor,ycor):
-        self.xcor=xcor
-        self.ycor=ycor
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.init()
         self.image=''
+        self.score=0
+        self.screenSize=(640,480)
+        self.screen=pygame.display.set_mode(self.screenSize)
+        self.change_x=0
+        self.change_y=0
+
 
     def __str__(self):
         string=""
@@ -15,12 +17,36 @@ class Player:
         string+="Y Coordinate: "+str(self.ycor)+"\n"
         return string
 
+    def changespeed(self,x,y):
+        self.change_x+=y
+        self.change_y+=y
+
+    def update(self):
+        self.rect.x+=self.change_x
+        self.rect.y+=self.change_y
+
     def movement(self,xcor,ycor):
-        self.xcor=xcor
-        self.ycor=ycor
-        #send in a direction, not an x and y coordinate. Moving one or two pixel at a time.
+        for event in pygame.event.get():
+            if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_LEFT:
+                    player.changespeed(-3,0)
+                if event.key==pygame.K_RIGHT:
+                    player.changespeed(3,0)
+            elif event.type==pygame.KEYUP:
+                if event.key==pygame.K_LEFT:
+                    player.changespeed(3,0)
+                if event.key==pygame.K_RIGHT:
+                    player.changespeed(-3,0)
+
+
+    def collision(self,zombie):
+        return self.rect.colliderect(zombie.rect)
 
     def death(self,zombie):
-        if self.xcor==zombie.xcor:
-            return ("game_over")
-        """return self.rect.colliderect(sprite.rect)"""
+        if self.colliderect(zombie):
+            del self
+        elif Bullet.ammo ==0:
+            del self
+
+        """if collision == True:
+                del player"""
