ea5e78335601f74ef5d40099efb21bb309f975bd
kylamel <kylameltzer@gmail.com>
2017-04-21 15:35:28 -0400

    Update ScoreClass.py

--- ScoreClass.py
+++ ScoreClass.py
@@ -1,13 +1,33 @@
 import time
-import random
 import pygame
-import threading
 
 class Score:
     def __init__(self,xcor,ycor,time):
         self.xcor=xcor
         self.ycor=ycor
         self.time=time
-        #writing to a file --> for data requirement 
+
+    def killZombie(self,zombie,bullet):
+        if pygame.sprite.spritecollide(bullet, zombie, True):
+            player.score+=1
+        return player.score
+
+    def survivalTime(self):
+        clock=pygame.time.Clock()
+        """sec=0
+        rate=60
+        total_seconds=sec//rate
+        minutes=total_seconds//60
+        seconds=total_seconds%60
+        time_string="Time: {0:02}:{1:02}".format(minutes,seconds)
+        text=font.render(time_string,True,BLACK)
+        screen.blit(text,[250,250])"""
+        while player.death == False:
+            return pygame.time.get_ticks()
+        if player.death==True:
+            pygame.time.wait()
+            return pygame.time.get_ticks()
+
+        #writing to a file --> for data requirement
 #Won't need an x and y coordinate, but would be handled by your view
-#This should keep track of time and high scores of how long people could stay alive 
+#This should keep track of time and high scores of how long people could stay alive
