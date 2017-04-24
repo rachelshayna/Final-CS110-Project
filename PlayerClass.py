3fee7c5b4700535e8c192e7363c3ea02f04b5406
lbirck1 <lbirck1@binghamton.edu>
2017-04-24 12:36:20 -0400

    Update PlayerClass.py

--- PlayerClass.py
+++ PlayerClass.py
@@ -2,14 +2,10 @@ import pygame
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
-        pygame.init()
         self.image=''
         self.score=0
-        self.screenSize=(640,480)
-        self.screen=pygame.display.set_mode(self.screenSize)
         self.change_x=0
         self.change_y=0
-
 
     def __str__(self):
         string=""
@@ -18,26 +14,12 @@ class Player(pygame.sprite.Sprite):
         return string
 
     def changespeed(self,x,y):
-        self.change_x+=y
-        self.change_y+=y
+        self.change_x+=x
+        
 
-    def update(self):
+    def update(self,x,y):
         self.rect.x+=self.change_x
         self.rect.y+=self.change_y
-
-    def movement(self,xcor,ycor):
-        for event in pygame.event.get():
-            if event.type==pygame.KEYDOWN:
-                if event.key==pygame.K_LEFT:
-                    player.changespeed(-3,0)
-                if event.key==pygame.K_RIGHT:
-                    player.changespeed(3,0)
-            elif event.type==pygame.KEYUP:
-                if event.key==pygame.K_LEFT:
-                    player.changespeed(3,0)
-                if event.key==pygame.K_RIGHT:
-                    player.changespeed(-3,0)
-
 
     def collision(self,zombie):
         return self.rect.colliderect(zombie.rect)
@@ -45,8 +27,12 @@ class Player(pygame.sprite.Sprite):
     def death(self,zombie):
         if self.colliderect(zombie):
             del self
+            return True
         elif Bullet.ammo ==0:
             del self
+            return True
+        else:
+            return False
 
         """if collision == True:
                 del player"""
