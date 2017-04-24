6e6c53fc10da8e20544abe9d170e66749d1f8a7d
kylamel <kylameltzer@gmail.com>
2017-04-24 13:34:34 -0400

    Update ScoreClass.py

--- ScoreClass.py
+++ ScoreClass.py
@@ -14,20 +14,8 @@ class Score:
 
     def survivalTime(self):
         clock=pygame.time.Clock()
-        """sec=0
-        rate=60
-        total_seconds=sec//rate
-        minutes=total_seconds//60
-        seconds=total_seconds%60
-        time_string="Time: {0:02}:{1:02}".format(minutes,seconds)
-        text=font.render(time_string,True,BLACK)
-        screen.blit(text,[250,250])"""
         while player.death == False:
             return pygame.time.get_ticks()
         if player.death==True:
             pygame.time.wait()
             return pygame.time.get_ticks()
-
-        #writing to a file --> for data requirement
-#Won't need an x and y coordinate, but would be handled by your view
-#This should keep track of time and high scores of how long people could stay alive
