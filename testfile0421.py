318fe615d7467a8cd1f76fb922145044de36d3f2
rachelshayna <rbass3@binghamton.edu>
2017-04-25 16:41:41 -0400

    Update testfile0421.py

--- testfile0421.py
+++ testfile0421.py
@@ -16,23 +16,16 @@ def __str__(self):
 return string
 
 def main(): 
-	#BulletClass() 
-	#ControllerClass()
-	#PlayerClass()
-	#ScoreClass()
-	#SpriteClass()
-	'''what is the sprite class doing?'''
-	#Zombies Class()
 	
 	Z1= ZombieClass.Zombies(10,10)
-	#print(Z1)
-	P = PlayerClass.Player(5,5)
+	print(Z1)
+	P = PlayerClass.Player() #does this work? 
 	P.movement(10,10)
 	print(P.death(Z1))
 	Zombie.__str___(Zombie) #is this the right formatting? Also aren't we supposed to avoid str functions?  
 	
 	Z2 = ZombieClass.Zombies(11,11)
-	#print(Z2)
+	print(Z2)
 	B = BulletClass.Bullet(17,17)
 	B.movement(11,11)
 	print(Z2.death(B))
