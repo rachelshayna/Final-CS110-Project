5e4d86c0f3b61518e26a0f32e94a12ac09d829e7
kylamel <kylameltzer@gmail.com>
2017-04-25 16:08:05 -0400

    Update ZombiesClass.py

--- ZombiesClass.py
+++ ZombiesClass.py
@@ -1,43 +1,32 @@
 import pygame
 import time
-import random
 
-class Zombies:
+class Zombie:
     def __init__(self,x,speed):
         self.x=x
         self.y=0
         self.image=pygame.image.load("""Image File""")
         self.rect=pygame.image.get_rect()
         self.speed=speed
-        zombies_group=[]
+        alive_zombies=zombies.sprites()
+        zombie_group=[zombie for zombie in zombies if bullet.kill()==False]
 
     def movement(self):
         self.rect=self.rect.move(self.speed)
 
 #spawn zombie left
     def spawnLeft(self):
-        zombies_group.append(Zombies,<ScreenSizeLeft>)
+        zombie_group.append(Zombie,<ScreenSizeLeft>) #might need to append tuple
+        update()
 
 #spawn zombie right
     def spawnRight(self):
-        zombies_group.append(Zombies,<ScreenSizeRight>)
-        
-#zombies spawn randomly on left and right at increasing intervals
-    def Spawn(self,zombie):
-        while player.death == False:
-            instance=1
-            faster=1
-            if score.survivalTime!=0:
-                random.choice(spawnLeft, spawnRight)*faster
-                instance+=1
-                if instance==5:
-                    instance=1
-                    faster+=1
-                    
+        zombie_group.append(Zombie,<ScreenSizeRight>)
+        update()
 
 #if spawn on left, go right
     def update(self):
-        for zombie in zombies_group:
+        for zombie in zombie_group:
             if zombie.spawnLeft:
                 zombie.changespeed(3,0)
             elif zombie.spawnRight:
@@ -46,4 +35,6 @@ class Zombies:
 #zombie dies
     def death(self,bullet):
         if self.rect.colliderect(bullet.rect):
-            zombies_group.remove()
+            zombie_group.remove(self) #or self.remove()
+            score.killZombie()
+
