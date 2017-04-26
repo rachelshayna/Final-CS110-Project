13fa76fe558d8232303cebefc69a8361cb9919b9
hchen113 <hchen113@binghamton.edu>
2017-04-26 00:05:36 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -17,6 +17,7 @@ background_image = pygame.image.load("background.png").convert()
 
 player_sprite = view_Player(800, 600)
 player = PlayerClass()
+
 all_sprites_list = pygame.sprite.Group()
 all_sprites_list.add(player_sprite)
 
@@ -28,20 +29,24 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-
+    # --- Keyboard Input ---
         elif event.type == pygame.KEYDOWN:
+                # --- Pressed Left Button---
                 if event.key == pygame.K_LEFT:
-                    if player.move(pygame.K_LEFT) == True:
-                        player_sprite.rect.x -= player_sprite.rect.width
-                    elif player.move(pygame.K_LEFT)!= False:
-                        player_sprite.direction(player)
+                    if player.move(pygame.K_LEFT) == True:                          # Move Left
+                        if player.moveLimit(pygame.K_LEFT, player_sprite) == True:  # Check if its point is on screen
+                            player_sprite.rect.x -= player_sprite.rect.width        # Move 100 px left
+                    elif player.move(pygame.K_LEFT)!= False:                        # Else
+                        player_sprite.direction(player)                             # Change Sprite Direction
+                # --- Pressed Right Button ---
                 elif event.key == pygame.K_RIGHT:
-                    if player.move(pygame.K_RIGHT) == True:
-                        player_sprite.rect.x += player_sprite.rect.width
-                    elif player.move(pygame.K_RIGHT)!= False:
-                        player_sprite.direction(player)
+                    if player.move(pygame.K_RIGHT) == True:                         # Move Right
+                        if player.moveLimit(pygame.K_RIGHT, player_sprite) == True: # Check if its point is on screen
+                            player_sprite.rect.x += player_sprite.rect.width        # Move 100 px right
+                    elif player.move(pygame.K_RIGHT)!= False:                       # Else
+                        player_sprite.direction(player)                             # Change Sprite Direction
 
-                elif event.key == pygame.K_SPACE:
+                #elif event.key == pygame.K_SPACE:
                     #player class code: Fires a bullet
 
         #elif event.type == #player alive = 0
