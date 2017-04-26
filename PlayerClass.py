a2d5004cf057b310855189f82351f6b855b5c98c
kylamel <kylameltzer@gmail.com>
2017-04-26 10:08:33 -0400

    Update PlayerClass.py

--- PlayerClass.py
+++ PlayerClass.py
@@ -1,36 +1,33 @@
 import pygame
 
-class PlayerClass():
+class Player(pygame.sprite.Sprite):
     def __init__(self):
-        self.direction = 'left'
+        self.direction='left'
 
+    def move(self,keypress):
+        if keypress==pygame.K_LEFT:
+            if self.direction=='left':
+                return True
+            if self.direction=='right':
+                self.direction='left'
+                return False
+        if keypress==pygame.K_RIGHT:
+            if self.direction=='right':
+                return True
+            if self.direction=='left':
+                self.direction='right'
+                return False
 
-    def move(self, keypress):
-        if keypress == pygame.K_LEFT:
-            if self.direction == 'left':
-                return True
-            if self.direction == 'right':
-                self.direction = 'left'
-                return False
-        if keypress == pygame.K_RIGHT:
-            if self.direction == 'right':
-                return True
-            if self.direction == 'left':
-                self.direction = 'right'
-                return False
-    def moveLimit(self, keypress, player):
-        if keypress == pygame.K_LEFT:
+    def moveLimit(self,keypress,player):
+        if keypress==pygame.K_LEFT:
             if player.rect.x >= 100:
                 return True
             else:
                 return False
-        elif keypress == pygame.K_RIGHT:
-            if player.rect.x <= 1400:
-                return True
-            else:
-                return False
+
     def direction(self):
         return(self.direction)
+
 #end game
     def death(self,zombie):
         if self.rect.colliderect(zombie.rect):
@@ -39,8 +36,3 @@ class PlayerClass():
             return True
         else:
             return False
-
-        
-      
-
-#find player direction; boolean value; check BV for player and set BV of the bullet ; then check bullets fired in event loop; if not empty, keep moving in whatever direction its facing; removed only if it one-hits a zombie or is outside window 
