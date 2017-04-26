9bb5812806bd8f8f3ccb9decc258a7908964ab5d
lbirck1 <lbirck1@binghamton.edu>
2017-04-26 11:58:35 -0400

    Update ZombiesClass.py

--- ZombiesClass.py
+++ ZombiesClass.py
@@ -2,17 +2,17 @@ import pygame
 import time
 
 class Zombie:
-    def __init__(self,x,speed):
-        self.x=x
-        self.y=0
-        self.image=pygame.image.load("""Image File""")
-        self.rect=pygame.image.get_rect()
-        self.speed=speed
+    def __init__(self,x):
+        self.x = x
+        #self.speed=speed
         alive_zombies=zombies.sprites()
-        zombie_group=[zombie for zombie in zombies if bullet.kill()==False]
+        #zombie_group=[zombie for zombie in zombies if bullet.kill()==False]
 
-    def movement(self):
-        self.rect=self.rect.move(self.speed)
+    def rightmovement(self):
+        zombie.rect.x += zombie.rect.width
+    
+    def leftmovement(self):
+        zombie.rect.x -= zombie.rect.width
 
 #spawn zombie left
     def spawnLeft(self):
@@ -20,21 +20,22 @@ class Zombie:
         update()
 
 #spawn zombie right
-    def spawnRight(self):
-        zombie_group.append(Zombie,<ScreenSizeRight>)
-        update()
+    #def spawnRight(self):
+        #zombie_group.append(Zombie,<ScreenSizeRight>)
+        #update()
 
 #if spawn on left, go right
-    def update(self):
-        for zombie in zombie_group:
-            if zombie.spawnLeft:
-                zombie.changespeed(3,0)
-            elif zombie.spawnRight:
-                zombie.changespeed(-3,0)
+    #def update(self):
+        #for zombie in zombie_group:
+            #if zombie.spawnLeft:
+                #zombie.changespeed(3,0)
+            #elif zombie.spawnRight:
+                #zombie.changespeed(-3,0)
 
 #zombie dies
-    def death(self,bullet):
+    def death(self, bullet):
         if self.rect.colliderect(bullet.rect):
-            zombie_group.remove(self) #or self.remove()
-            score.killZombie()
+            self.remove() #or self.remove()
+            zombie_sprite.remove()
+                #update score
 
