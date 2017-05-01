a231b7a573e730967fceadc3d989167102dcc127
lbirck1 <lbirck1@binghamton.edu>
2017-05-01 15:43:13 -0400

    Update highscore.py

--- highscore.py
+++ highscore.py
@@ -1,1 +1,10 @@
-
+def highscore(score):
+  infile = open("hiscore.txt", "r")
+  old_hi = int(infile.readline())
+  infile.close()
+  outfile = open("hiscore.txt", "w")
+  if old_hi > score:
+    outfile.write(str(old_hi))
+    return old_hi
+  outfile.write(str(score))
+  return score
